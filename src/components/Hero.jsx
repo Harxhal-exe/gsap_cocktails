0077dccf8e7391234086581d29147f20d6815c11
this_harxhal <harshalbhoge4@gmail.com>
2026-02-10 22:43:49 +0530

    Implement Hero Scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,15 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
-import { SplitText } from "gsap/all"
+import { ScrollTrigger, SplitText } from "gsap/all"
+import { useRef } from "react"
+import { useMediaQuery } from "react-responsive"
 
-gsap.registerPlugin(SplitText)
+gsap.registerPlugin(SplitText, ScrollTrigger)
 
 const Hero = () => {
+  const videoRef = useRef()
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
   useGSAP(() => {
     const heroSplit = new SplitText('.title', { type: 'chars, words' })
@@ -17,7 +22,7 @@ const Hero = () => {
       duration: 1.8,
       opacity: 0,
       ease: 'expo.out',
-      stagger: 0.06 
+      stagger: 0.06
     });
 
     gsap.from(paragraphSplit.lines, {
@@ -34,11 +39,31 @@ const Hero = () => {
         trigger: '#hero',
         start: 'top top',
         end: 'bottom top',
-        scrub: true, 
+        scrub: true,
       }
     })
-    .to('.right-leaf', { y: 200 }, 0)
-    .to('.left-leaf', { y: -200 }, 0)
+      .to('.right-leaf', { y: 200 }, 0)
+      .to('.left-leaf', { y: -200 }, 0)
+
+    const startValue = isMobile ? 'top 50%' : 'center 60%'
+    const endValue = isMobile ? '120% top' : 'bottom top'
+
+    // Video scroll animation timeline
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'video',
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      }
+    })
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration
+      })
+    }
   }, [])
 
   return (
@@ -67,6 +92,17 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+
+      </div>
     </>
   )
 }
